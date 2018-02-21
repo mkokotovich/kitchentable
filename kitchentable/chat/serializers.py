100bc6a0c312d554c3284984a1f3539c56437811
Matthew Kokotovich <mkokotovich@gmail.com>
2018-02-21 16:09:41 -0600

    Improves results of post list by adding more details about the likes

--- kitchentable/chat/serializers.py
+++ kitchentable/chat/serializers.py
@@ -16,14 +16,27 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'name')
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('url', 'owner', 'content', 'likes')
-        read_only_fields = ('likes',)
-
-
 class LikeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Like
         fields = ('url', 'owner', 'post')
+
+
+class UserSummarySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username')
+
+
+class LikeInfoSerializer(serializers.HyperlinkedModelSerializer):
+    owner = UserSummarySerializer(many=False, read_only=True)
+    class Meta:
+        model = Like
+        fields = ('url', 'owner')
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    likes = LikeInfoSerializer(many=True, read_only=True)
+    class Meta:
+        model = Post
+        fields = ('url', 'owner', 'content', 'likes')
