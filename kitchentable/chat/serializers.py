3f6f0c8c536310038793a0bee64cf67a5623ad7e
Matthew Kokotovich <mkokotovich@gmail.com>
2018-02-21 15:51:10 -0600

    Improves relationship between likes and posts

--- kitchentable/chat/serializers.py
+++ kitchentable/chat/serializers.py
@@ -6,7 +6,7 @@ from kitchentable.chat.models import *
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups', 'posts')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,7 +18,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('url', 'owner', 'content')
+        fields = ('url', 'owner', 'content', 'likes')
+        read_only_fields = ( 'likes', )
 
 
 class LikeSerializer(serializers.HyperlinkedModelSerializer):
